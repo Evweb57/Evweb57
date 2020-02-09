cfdf32aaaa9b166fc9e10ae13de2a903134d2a12
Eric <Ericvanmeter57@gmail.com>
2020-02-09 14:40:22 -0600

    created tab display, does not work

--- spoonacular_app/js/app.js
+++ spoonacular_app/js/app.js
@@ -21,6 +21,32 @@ const fetchRecipe = () => {
 }
 
 /*==============================================================================
+  Tab Panel
+==============================================================================*/
+
+$('.tab-list').each(function(){
+  var $this = $(this);
+  var $tab = $this.find('li.active');
+  var $link = $tab.find('a');
+  var $panel = $($link.attr('href'));
+
+  $this.on('click', '.tab-control', function(e) {
+    console.log('clicked');
+    e.preventDefault();
+    var $link = $(this);
+    var id = this.hash;
+
+    if (id && !$link.is('.active')) {
+      $panel.removeClass('active');
+      $tab.removeClass('active');
+
+      $panel = $(id).addClass('active');
+      $tab = $link.parent().addClass('active');
+    }
+  });
+});
+
+/*==============================================================================
   Onload functions
 ==============================================================================*/
 $(() => {
