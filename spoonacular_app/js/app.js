ac0960fc9b0d9784a0dd333098f4c6843d38d30f
Eric <Ericvanmeter57@gmail.com>
2020-02-10 09:59:52 -0600

    building on funtionality of fetchRecipe

--- spoonacular_app/js/app.js
+++ spoonacular_app/js/app.js
@@ -3,22 +3,29 @@
   Setting up Spoonacular server response - f308c68870684766ae3372174c399412
 ==============================================================================*/
 const myApiKey = "apiKey=f308c68870684766ae3372174c399412";
-let userInput = "&query=cheese&number=2";
 
 const fetchRecipe = () => {
-  const myData = $.ajax({
-    url: "https://api.spoonacular.com/recipes/search?" + myApiKey + userInput
-  }).then(
-    (data)=>{
-      // $('#test').html(data.)
-      console.log("good");
-      console.log(data);
-    },
-    ()=>{
-      console.log('bad');
-    }
-  );
+  $('form').on('submit', (event) => {
+    event.preventDefault();
+    let selection = document.activeElement.getAttribute('value');
+    const userInput = $('.search-box').val()
+    const userSearch = `&query=${userInput}&number=10`;
+    console.log(userSearch);
+    const myData = $.ajax({
+      url: "https://api.spoonacular.com/recipes/search?" + myApiKey + userSearch
+    }).then(
+      (data)=>{
+        // $('#test').html(data.)
+        console.log("good");
+      },
+      ()=>{
+        console.log('bad');
+      }
+    );
+  })
 }
+
+
 
 /*==============================================================================
   Tab Panel
@@ -52,6 +59,6 @@ const searchTabs = () => {
   Onload functions
 ==============================================================================*/
 $(() => {
-  // fetchRecipe();
+  fetchRecipe();
   searchTabs();
 })
